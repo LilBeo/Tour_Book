80891907f080cf1c7ec834205ab40f8d1a2dac8f
DELL <DELL@DESKTOP-BN6NO1L>
2025-10-14 01:00:27 +0700

    Add project files.

---
+++ Controllers/TourController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tour_Booking.Controllers
+{
+    public class TourController : Controller
+    {
+        // GET: Tour
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult Detail()
+        {
+            return View();
+        }
+        public ActionResult Plan()
+        {
+            return View();
+        }
+        public ActionResult Location()
+        {
+            return View();
+        }
+        public ActionResult Gallery()
+        {
+            return View();
+        }
+        public ActionResult Pay()
+        {
+            return View();
+        }
+        public ActionResult Travel()
+        {
+            return View();
+        }
+        public ActionResult Aboutus()
+        {
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tour_Book.MvcApplication" Language="C#" %>
